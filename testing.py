c358ffea3666ba8fe3b2aa7a29f663e94d1fb4d2
perspect1va <gavnn31@tutamail.com>
2025-06-19 14:29:45 +0300

    Add files via upload

--- testing.py
+++ testing.py
@@ -2,32 +2,35 @@ from testing_lz import gang
 from testing2 import gang_man_tests
 import unittest
 
-#Автомат тесты
 class Test_Equalize(unittest.TestCase):
-
     def test_valid_1(self):
-        self.assertAlmostEqual(gang(2, 3, 4), 0.30694266481558556, places=6)
+        result = gang(2, 3, 1)
+        if isinstance(result, str):
+            self.fail(f"Ожидалось число, получена ошибка: {result}")
+        self.assertAlmostEqual(result, 0.24012907173427356, places=6)
 
     def test_valid_2(self):
-        self.assertAlmostEqual(gang(5, 2, 1), 1.448147589853455, places=6)
+        result = gang(5, 2, 1)
+        if isinstance(result, str):
+            self.fail(f"Ожидалось число, получена ошибка: {result}")
+        self.assertAlmostEqual(result, 1.448147589853455, places=6)
 
     def test_log_domain_error(self):
-        with self.assertRaises(ValueError):
-            gang(0.5, 2, 1)
-
+        result = gang(0, 2, 1)
+        self.assertIsInstance(result, str)
+        self.assertIn("", result)
+           
     def test_div_zero_1(self):
-        with self.assertRaises(ZeroDivisionError):
-            gang(3, 2, 4)
-
-    def test_div_zero_2(self):
-        with self.assertRaises(ZeroDivisionError):
-            gang(10, 5, 5)
+        result = gang(2, 2, 2)
+        self.assertIsInstance(result, str)
+        self.assertIn("Ошибка при вычислении: Знаменатель левого члена равен 0", result)
 
     def test_sqrt_domain_error(self):
-        with self.assertRaises(ValueError):
-            gang(1, 2, 3)
+        result = gang(1, 2, 3)
+        self.assertIsInstance(result, str)
+        self.assertIn("Выражение под корнем должно быть положительным", result)
 
 if __name__ == '__main__':
     gang_man_tests()
     print("Автоматическое тестирование:\n")
-    unittest.main()    
+    unittest.main()
--- testing2.py
+++ testing2.py
@@ -1,17 +1,17 @@
 from testing_lz import gang
 
-#Ручн тестирование
 def gang_man_tests():
     print("Ручное тестирование:")
     test_cases = [
-        (2, 3, 4),
-        (1, 2, 3),
-        (3, 2, 4),
-        (0.5, 2, 1),
-        (10, 5, 5),
-        (5, 2, 1)
+        (2, 3, 1),    
+        (0, 2, 1),    
+        (1, 2, 3),    
+        (2, 2, 2),    
+        (0.5, 2, 1),   
+        (5, 2, 1)      
     ]
-    for i, (a, b, c) in enumerate(test_cases, start=1):
+    
+    for i, (a, b, c) in enumerate(test_cases, 1):
         result = gang(a, b, c)
         print(f"Тест {i}: a={a}, b={b}, c={c} → {result}")
     print()
